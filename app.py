e554bac970d9358a65b79a4869e3afc0fcde948a
Manelitu <emanuelwork1@gmail.com>
2024-09-04 21:44:59 -0300

    feat: create list all users method

--- app.py
+++ app.py
@@ -30,8 +30,18 @@ def list_users():
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_ALL_USERS)
-            users = cursor.fetchone()[0]
-    return {"users": users }, 200     
+            users = cursor.fetchall()
+            print(users)
+            response = []
+            for user in users:
+                each_user = {
+                    "id": user[0],
+                    "name": user[1],
+                    "email": user[2],
+                    "password": user[3]
+                }
+                response.append(each_user)
+    return response, 200     
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
